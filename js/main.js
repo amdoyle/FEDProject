23b299f5a3a2e313110fd49b0ff5b530fbf25129
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-08-08 18:41:00 -0400

    Add jQuery and add toggle to hamburger nav on mobile view

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-// Your code here ...
+// Your code here
+$( document ).ready(function() {
+
+  // Hiding nav links on mobile
+  $('#nav-links').hide();
+
+  // On click toggle the nav links
+  $('#hamburger-nav').on('click', function(){
+    $('#nav-links').toggle();
+  });
+
+
+});
