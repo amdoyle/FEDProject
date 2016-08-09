0f363a5aab86bf0afc65944475d6dff7bfe30e3f
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-08-09 00:03:07 -0400

    Add all place holder images and majority of mobile styles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Your code here
-$( document ).ready(function() {
+$(document).ready(function() {
 
-  // Hiding nav links on mobile
-  $('#nav-links').hide();
-
+  // Hiding nav links on screens smaller than 1000px
+  if($(window).width() < 1000){
+      $('#nav-links').hide();
+  }
   // On click toggle the nav links
   $('#hamburger-nav').on('click', function(){
     $('#nav-links').toggle();
   });
-
 
 });
