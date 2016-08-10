b187d121866d19176accdc9e3785e96f1be87df3
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-08-10 15:26:38 -0400

    Implimented slick image slider and adjusted styles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,69 @@
 // Your code here
 $(document).ready(function() {
 
-  // Hiding nav links on screens smaller than 1000px
-  if($(window).width() < 1000){
-      $('#nav-links').hide();
-  }
+  showNav();
+
+  $(window).resize(function(){
+    showNav();
+  });
+
   // On click toggle the nav links
   $('#hamburger-nav').on('click', function(){
     $('#nav-links').toggle();
   });
+
+
+
+// Hiding nav links on screens smaller than 1000px
+function showNav(){
+  if($(window).width() < 1200){
+    $('#nav-links').hide();
+    $('#hamburger-nav').show();
+  } else {
+    $('#nav-links').show();
+    $('#hamburger-nav').hide();
+  }
+}
+
+$("#slider").slick({
+        dots: true,
+        pauseOnDotsHover: true,
+        arrows: false,
+        autoplay: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+//
+//   // normal options...
+//   infinite: false,
+//
+//   // the magic
+//   responsive: [{
+//
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         infinite: true
+//       }
+//
+//     },
+//      {
+//
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 1,
+//         dots: true
+//       }
+//
+//     },
+//      {
+//
+//       breakpoint: 300,
+//       settings: "unslick" // destroys slick
+//
+//     }]
+// });
 
 });
