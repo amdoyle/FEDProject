ee63be214370d315ca5348890fc8d6fddc407ad8
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-08-11 14:42:15 -0400

    Add functionality to submit seach query on enter

---
+++ README.md
@@ -0,0 +1,2 @@
+jQuery
+Slick (plug in for slider)
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Your code here
 $(document).ready(function() {
 
+///////// NAV /////////
   showNav();
 
   $(window).resize(function(){
@@ -13,32 +14,50 @@ $(document).ready(function() {
   });
 
 
-
-// Hiding nav links on screens smaller than 1000px
-function showNav(){
-  if($(window).width() < 1200){
-    $('#nav-links').hide();
-    $('#hamburger-nav').show();
-  } else {
-    $('#nav-links').show();
-    $('#hamburger-nav').hide();
+  // Hiding nav links on screens smaller than 1000px
+  function showNav(){
+    if($(window).width() < 1200){
+      $('#nav-links').hide();
+      $('#hamburger-nav').show();
+    } else {
+      $('#nav-links').show();
+      $('#hamburger-nav').hide();
+    }
   }
-}
 
-$("#slider").slick({
-        dots: true,
-        mobileFirst: true,
-        pauseOnDotsHover: true,
-        arrows: false,
-        autoplay: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        zIndex: 0,
-        swipe: true,
-        respondTo: 'window'
+/////////  SLIDER /////////
+  $("#slider").slick({
+    dots: true,
+    mobileFirst: true,
+    pauseOnDotsHover: true,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    zIndex: 0,
+    swipe: true,
+    respondTo: 'window'
+  });
+
+///////// SEARCH FUNCTIONS /////////
+    $('#searchForm').keydown(function(e) {
+       var key = e.which;
+
+       if(key === 13){  // the enter key code
+         $(this).submit();
+       }
     });
 
+    $('#searchForm').on('submit', function(e) {
+      e.preventDefault();
+      search();
+    });
+
+
+  function search() {
+
+  }
 
 });
